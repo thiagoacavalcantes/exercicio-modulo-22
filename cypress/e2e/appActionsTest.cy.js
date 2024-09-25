9bca1586b2ab92fff69de89dae1cf30dbd31194c
Thiago Cavalcante <thiagoacavalcante_@hotmail.com>
2024-09-25 15:30:40 -0300

    Correção exercicio

--- cypress/e2e/appActionsTest.cy.js
+++ cypress/e2e/appActionsTest.cy.js
@@ -8,7 +8,8 @@ describe('Validar fluxo do carrinho de compras com AppActions', () => {
 
     it('Deve finalizar uma compra de carrinho de comprar', () => {
        cy.carrinho()
-       cy.get(':nth-child(3) > :nth-child(1) > :nth-child(1) > :nth-child(1) > :nth-child(1) > [style="background-color: rgb(242, 242, 242); display: flex;"] > :nth-child(1) > :nth-child(1) > .r-13awgt0 > :nth-child(1) > .css-175oi2r > .css-146c3p1').should('contain', 'Order Success') 
+       cy.contains('Order Success').should('have.attr', 'class', 'css-146c3p1')
+       
        
     });
 });
